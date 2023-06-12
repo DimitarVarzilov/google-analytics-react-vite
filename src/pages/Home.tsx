d7abb6b2608b5bb04197855aaa45f63fc934a571
DimitarVarzilov <dnvarzilov@diadraw.com>
2023-06-12 10:48:13 +0300

    icons fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,8 @@ const Home = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <img className="logo" src="./vite.svg" alt="Vite icon" />
-      <img className="logo" src="./react.png" alt="React icon" />
+      <img className="logo" src="/vite.svg" alt="Vite icon" />
+      <img className="logo" src="/react.svg" alt="React icon" />
       <h1>Vite + React</h1>
       <div className="card">
         <button
